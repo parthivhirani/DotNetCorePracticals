c9f7fe6a0503f4c8c888fe097ad50bd7163140dd
CorePracticals <parthivhirani2903@gmail.com>
2023-07-05 20:01:49 +0530

    ASP .NET Core Practical-16

---
+++ CorePractical16/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CorePractical16.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+                _logger = logger;
+        }
+
+
+        [HttpGet]
+        public string Get()
+        {
+            _logger.LogInformation("Calling Get() method of HelloWorld api");
+            return "Hello World";
+        }
+    }
+}
